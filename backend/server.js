3cb94768f8c3572c8ab86753ebcefa49e22017ee
Filip Ivanovic <filip_ivanovic@hotmail.com>
2025-04-25 23:44:56 +0200

    routes and controller implemented

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import authRoutes from './routes/auth.route.js'
+
+dotenv.config()
+const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+app.get('/', (req, res) => {
+  // root route http://localhost:5000/
+  res.send('Hello World!')
+})
+
+app.use('/api/auth', authRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
