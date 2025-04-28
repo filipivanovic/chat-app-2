ab747f42cb47817cde49864c77979791220b91c7
Filip Ivanovic <filip_ivanovic@hotmail.com>
2025-04-28 19:36:01 +0200

    NoChatSelected functionality added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
-import Login from './pages/login/Login.jsx'
-import SignUp from './pages/signup/SignUp.jsx'
+
 import Home from './pages/home/Home.jsx'
 
 function App() {
@@ -8,8 +7,6 @@ function App() {
     <>
       <div className="p-4 h-screen flex items-center justify-center">
         <Home />
-        {/*<Login />*/}
-        {/*<SignUp />*/}
       </div>
     </>
   )
