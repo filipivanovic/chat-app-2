472466e83625e386eb3447032ade6527288c9896
Filip Ivanovic <filip_ivanovic@hotmail.com>
2025-04-29 23:53:48 +0200

    Routes instalation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
 
 import Home from './pages/home/Home.jsx'
+import Login from './pages/login/Login.jsx'
+import SignUp from './pages/signup/SignUp.jsx'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <>
       <div className="p-4 h-screen flex items-center justify-center">
-        <Home />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
       </div>
     </>
   )
