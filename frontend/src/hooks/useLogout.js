cb514e37b626b29d18291b9ae386a31899bec437
Filip Ivanovic <filip_ivanovic@hotmail.com>
2025-04-30 23:18:53 +0200

    Logout functionality implemented

---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import { useAuthContext } from '../context/AuthContext.jsx'
+import toast from 'react-hot-toast'
+
+const useLogout = () => {
+  const [loading, setLoading] = useState(false)
+  const { setAuthUser } = useAuthContext()
+
+  const logout = async () => {
+    setLoading(true)
+    try {
+      const res = await fetch('/api/auth/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+      })
+      const data = await res.json()
+
+      if (data.error) {
+        throw new Error(data.error)
+      }
+
+      localStorage.removeItem('chat-user')
+      setAuthUser(null)
+    } catch (error) {
+      console.error(`Error in logout method: ${error.message || error}`)
+      toast.error(error.message || error || 'Internal Server Error')
+    } finally {
+      setLoading(false)
+    }
+  }
+  return {
+    logout,
+    loading
+  }
+}
+
+export default useLogout
