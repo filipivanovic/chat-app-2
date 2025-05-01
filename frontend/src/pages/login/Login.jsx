e430e59cf54aeb2c233522444c9fc79a50b56c0f
Filip Ivanovic <filip_ivanovic@hotmail.com>
2025-05-01 11:08:33 +0200

    login functionality completed

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,6 +1,18 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import useLogin from '../../hooks/useLogin.js'
 
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { loading, login } = useLogin()
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    await login(username, password)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -9,15 +21,17 @@ const Login = () => {
           <span className="text-blue-500"> ChatApp</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="text-base label-text">Username</span>
             </label>
             <input
               type="text"
+              value={username}
               placeholder="Enter username"
               className="w-full input input-bordered h-10"
+              onChange={e => setUsername(e.target.value)}
             />
           </div>
 
@@ -27,8 +41,10 @@ const Login = () => {
             </label>
             <input
               type="password"
+              value={password}
               placeholder="Enter Password"
               className="w-full input input-bordered h-10"
+              onChange={e => setPassword(e.target.value)}
             />
           </div>
           <Link
@@ -39,7 +55,9 @@ const Login = () => {
           </Link>
 
           <div>
-            <button className="btn btn-block btn-sm mt-2">Login</button>
+            <button className="btn btn-block btn-sm mt-2" disabled={loading}>
+              {loading ? <span className="loading loading-spinner"></span> : 'Login'}
+            </button>
           </div>
         </form>
       </div>
